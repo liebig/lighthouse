4e06b7c76b551ed7601988abb8b5fdc28b2bf700
Marc Liebig <marc.liebig@gmx.de>
2017-07-16 14:03:37 +0200

    Renaming Spring Boot application class

--- src/main/java/de/liebig/lighthouse/Application.java
+++ src/main/java/de/liebig/lighthouse/Application.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class LighthouseApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(LighthouseApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 }
